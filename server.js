316a117ff26f6a4878a2b2a9587c615570b6b303
Juan Carlos Rudin <juan.rudin@drivedominion.com>
2018-07-31 11:12:28 -0600

    Heroku deploy 5.

--- server.js
+++ server.js
@@ -5,12 +5,10 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-//app.use(express.static(__dirname + '/dist/gym-web-app'));
-app.use(express.static('build'));
+app.use(express.static(__dirname + '/dist'));
 
 app.get('/*', function(req,res) {
-    
-res.sendFile('index.html');
+    res.sendFile(path.join(__dirname+'/dist/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
