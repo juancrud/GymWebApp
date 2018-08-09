583d308e2c3b90fd309f0ebcd2120e3011e6f8d1
Juan Carlos Rudin <juan.rudin@drivedominion.com>
2018-08-09 15:09:56 -0600

    DashboardComponent.

--- src/app/admin/dashboard/dashboard.component.ts
+++ src/app/admin/dashboard/dashboard.component.ts
@@ -1,4 +1,9 @@
 import { Component, OnInit } from '@angular/core';
+import { CustomerService } from '../../core/services/customer.service';
+import { TrainerService } from '../../core/services/trainer.service';
+import { MeasurementService } from '../../core/services/measurement.service';
+import { TrainerStatus } from '../../core/models/Trainer';
+import { CustomerStatus } from '../../core/models/Customer';
 
 @Component({
   selector: 'app-dashboard',
@@ -6,10 +11,42 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
+  totalTrainers: number;
+  activeTrainers: number;
+  inactiveTrainers: number;
+  deletedTrainers: number;
 
-  constructor() { }
+  totalCustomers: number;
+  activeCustomers: number;
+  inactiveCustomers: number;
+  deletedCustomers: number;
+
+  totalMeasurements: number;
+
+  constructor(
+    private customerService: CustomerService,
+    private trainerService: TrainerService,
+    private measurementService: MeasurementService
+  ) { }
 
   ngOnInit() {
+    this.trainerService.getTrainers().subscribe(trainers => {
+      this.totalTrainers = trainers.length;
+      this.activeTrainers = trainers.filter(t => t.status === TrainerStatus.Active).length;
+      this.inactiveTrainers = trainers.filter(t => t.status === TrainerStatus.Inactive).length;
+      this.deletedTrainers = trainers.filter(t => t.status === TrainerStatus.Deleted).length;
+    });
+
+    this.customerService.getCustomers().subscribe(customers => {
+      this.totalCustomers = customers.length;
+      this.activeCustomers = customers.filter(t => t.status === CustomerStatus.Active).length;
+      this.inactiveCustomers = customers.filter(t => t.status === CustomerStatus.Inactive).length;
+      this.deletedCustomers = customers.filter(t => t.status === CustomerStatus.Deleted).length;
+    })
+
+    this.measurementService.getMeasurements().subscribe(measurements => {
+      this.totalMeasurements = measurements.length;
+    })
   }
 
 }
