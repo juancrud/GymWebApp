bff97ce8b59edae4d1ba2f05d11f5c579c6727ae
Gabriela Sanchez <gabskatze@gmail.com>
2018-08-30 08:07:44 -0600

    forms design

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,12 +3,10 @@ import { BrowserModule } from '@angular/platform-browser';
 import { CoreModule } from './core/core.module';
 import { AngularFontAwesomeModule } from 'angular-font-awesome';
 import { AppComponent } from './app.component';
-import { LoginComponent } from './app/login/login.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    LoginComponent,
   ],
   imports: [
     BrowserModule,
