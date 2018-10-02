2b43624ad1e8867ae7a1054dbfdaf23cf4b481db
Juan Carlos Rudin <juan.rudin@drivedominion.com>
2018-10-02 16:14:25 -0600

    Adding new shared module

---
+++ src/app/shared/sidebar/sidebar.component.ts
@@ -0,0 +1,21 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-sidebar',
+  templateUrl: './sidebar.component.html',
+  styleUrls: ['./sidebar.component.scss']
+})
+export class SidebarComponent implements OnInit {
+  @Input() items: SidebarItem[];
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
+
+export class SidebarItem {
+  label: string;
+  link: string;
+}
