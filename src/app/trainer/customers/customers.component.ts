0087ffa25801f19aaee1a21eb2af8fb29891abfa
Gabriela Sanchez <gabskatze@gmail.com>
2018-10-23 09:43:07 -0600

    .

--- src/app/trainer/customers/customers.component.ts
+++ src/app/trainer/customers/customers.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { Customer, CustomerStatus } from '../../core/models/Customer';
+import { CustomerService } from '../../core/services/customer.service';
+import { Mapper } from '../../core/mappers/Mapper';
+import { GridModel } from '../../core/models/GridModel';
 
 @Component({
   selector: 'app-customers',
@@ -6,10 +10,17 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./customers.component.scss']
 })
 export class CustomersComponent implements OnInit {
+  customers: Customer[];
+  error: Error;
 
-  constructor() { }
+  constructor(private customerService: CustomerService, private mapper: Mapper) { }
 
   ngOnInit() {
+    this.customerService.getCustomers()
+      .subscribe(customers => this.customers = customers, error => this.error = error);
   }
 
+  mapCustomers(): GridModel[] {
+    return this.customers.map(x => this.mapper.mapCustomerToGridModel(x))
+  }
 }
